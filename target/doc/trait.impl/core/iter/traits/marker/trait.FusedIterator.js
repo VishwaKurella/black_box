bd7ba7b44ffc847632607bbd4e13c6cb44498661
Vishwanath Kurella <vishwa.kurella@gmail.com>
2025-11-01 23:39:01 +0530

    Removed target folder

--- target/doc/trait.impl/core/iter/traits/marker/trait.FusedIterator.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["chrono",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"chrono/naive/struct.NaiveDateDaysIterator.html\" title=\"struct chrono::naive::NaiveDateDaysIterator\">NaiveDateDaysIterator</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"chrono/naive/struct.NaiveDateWeeksIterator.html\" title=\"struct chrono::naive::NaiveDateWeeksIterator\">NaiveDateWeeksIterator</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[725]}
